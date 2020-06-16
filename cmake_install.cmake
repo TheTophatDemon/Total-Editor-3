e28da00ec2c385bfc770267bf21211ee07bc7eae
TheTophatDemon <andsotherewas@gmail.com>
2020-06-16 14:23:21 -0500

    Attempted to fix untracked files interference with .gitignore

--- cmake_install.cmake
+++ cmake_install.cmake
@@ -35,6 +35,7 @@ endif()
 if(NOT CMAKE_INSTALL_LOCAL_ONLY)
   # Include the install script for each subdirectory.
   include("D:/Game Projects/C++/Total Editor 3/imgui/cmake_install.cmake")
+  include("D:/Game Projects/C++/Total Editor 3/glm/cmake_install.cmake")
 
 endif()
 
