6b4739a02043a36a741da2729943a59febd3f7d1
TheTophatDemon <andsotherewas@gmail.com>
2020-06-18 10:37:55 -0500

    Implemented virtual update method. Added new constructor.

--- camera.h
+++ camera.h
@@ -9,7 +9,11 @@ class Camera : Node
 {
 public:
     //Fov is in degrees
+    Camera(const glm::vec3 pos, const glm::quat rot, const float fov, const float aspectRatio, const float near, const float far);
+    //Fov is in degrees
     Camera(const float fov, const float aspectRatio, const float near, const float far);
+
+    void update(const float deltaTime) override;
 
     inline const glm::mat4x4 getViewProjectionMatrix()
     {
